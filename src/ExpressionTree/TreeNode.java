2907a6023c58041d1abbf070531870ddaa66571b
Abraham Venegas Mayorga <85382606+AbrahamVenegas@users.noreply.github.com>
2021-11-06 00:22:56 -0600

    Update TreeNode.java

--- src/ExpressionTree/TreeNode.java
+++ src/ExpressionTree/TreeNode.java
@@ -1,9 +1,13 @@
 package ExpressionTree;
 
+/**
+     * Class for the tree node in charge of getting and reading the elements of the tree
+     */
+
 public class TreeNode<T> {
     T element;
     TreeNode<T> left;
-    TreeNode<T> right;
+    TreeNode<T> right; // we set the variables
 
     public TreeNode(T element) {
         this(element, null, null);
@@ -15,19 +19,19 @@ public class TreeNode<T> {
         this.right = right;
     }
 
-    public T getElement() {
+    public T getElement() { // we start on the first element
         return element;
     }
 
-    public TreeNode<T> getLeft() {
+    public TreeNode<T> getLeft() { // if we get on the left it gets the left
         return left;
     }
 
-    public TreeNode<T> getRight() {
+    public TreeNode<T> getRight() { // if we get on the right it gets the right
         return right;
     }
 
-    public void setElement(T element) {
+    public void setElement(T element) { // we set the elements on the left or right
         this.element = element;
     }
 
