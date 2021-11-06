78afc18df42dca3a05cd9d56396f5765ac2bcec3
Abraham Venegas Mayorga <85382606+AbrahamVenegas@users.noreply.github.com>
2021-11-06 00:14:20 -0600

    Update csvReaderPrinter.java

--- src/CSV/csvReaderPrinter.java
+++ src/CSV/csvReaderPrinter.java
@@ -2,10 +2,16 @@ package CSV;
 
 import java.io.*;
 
-public class csvReaderPrinter {
+/**
+     * Class in charge of writing on the csv document of each user
+     */
 
+public class csvReaderPrinter {
+/**
+     * the function saves the data for each user and not for all
+     */
     public String historyUser(String username) {
-        String filepath = username+".csv";
+        String filepath = username+".csv"; // it makes a new document with the username of the person that's using the calculator
         BufferedReader reader = null;
         String line = "";
         String operations = "";
@@ -19,23 +25,23 @@ public class csvReaderPrinter {
                     tempOperations += index+",";
                 }
                 String[] temp = tempOperations.split(",");
-                operations += "Your expression: "+temp[0]+"  Result: "+ temp[1]+" Date: "+temp[2]+"\n";
+                operations += "Your expression: "+temp[0]+"  Result: "+ temp[1]+" Date: "+temp[2]+"\n"; // writes the values for the operation
                 tempOperations = "";
             }
             System.out.println(operations);
             if (reader != null){
                 try {
-                    reader.close();
+                    reader.close(); // if there isn't any operations it stops the function
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return operations;
         } catch (FileNotFoundException f){
-            return "There aren't expressions.";
+            return "There aren't expressions."; // it shows a warning
         } catch(Exception e){
             e.printStackTrace();
         }
-        return "There aren't expressions.";
+        return "There aren't expressions."; // it shows a warning
     }
 }
