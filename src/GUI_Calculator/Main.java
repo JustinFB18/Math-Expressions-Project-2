a50cf034836fe7937f5dc9420e1fbf64e1eaef58
Abraham Venegas Mayorga <85382606+AbrahamVenegas@users.noreply.github.com>
2021-11-06 00:28:24 -0600

    Update Main.java

--- src/GUI_Calculator/Main.java
+++ src/GUI_Calculator/Main.java
@@ -6,14 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Class for the main code that starts the interface
+ */
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("GUI_Calculator.fxml"));
-        primaryStage.setTitle("Math Expressions");
-        primaryStage.setScene(new Scene(root, 400, 430));
-        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("GUI_Calculator.fxml")); 
+        primaryStage.setTitle("Math Expressions");  // the title of the project on the interface
+        primaryStage.setScene(new Scene(root, 400, 430));  // the measurements of the interface
+        primaryStage.show(); // it shows the interface
     }
     public static void main(String[] args) { launch(args); }
 }
