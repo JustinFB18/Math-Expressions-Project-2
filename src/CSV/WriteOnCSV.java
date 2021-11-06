1cb15b5187931d3ae6e836b00f65573d33ec35b5
Abraham Venegas Mayorga <85382606+AbrahamVenegas@users.noreply.github.com>
2021-11-05 23:54:52 -0600

    Update WriteOnCSV.java

--- src/CSV/WriteOnCSV.java
+++ src/CSV/WriteOnCSV.java
@@ -5,23 +5,28 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 
+/**
+     * Clase encargada de escribir los datos en el documento csv de cada usuario
+     */
+
 public class WriteOnCSV {
 
+    // la función permite guardar los tres datos que se nos solicita: expresión, resultado y fecha
     public static void saveRecord(String Expression, String Result, String filepath) {
         try{
             FileWriter fw = new FileWriter(filepath, true);
             BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(fw);
+            PrintWriter pw = new PrintWriter(fw);  // parámetros que nos permiten escribir en el archivo
 
-            String Date = String.valueOf(LocalDate.now());
-            Date = Date.substring(2);
-            pw.println(Expression+","+Result+","+Date);
+            String Date = String.valueOf(LocalDate.now()); // escribe la fecha del día en que se haga la operación
+            Date = Date.substring(2);  // corta la fecha para que no aparezca lahora
+            pw.println(Expression+","+Result+","+Date);  // aquí escribe los tres datos necesarios
             pw.flush();
             pw.close();
 
         }
         catch(Exception E){
-            System.out.println("Error writing on file");
+            System.out.println("Error writing on file");  // en caso de haber un error nos da un aviso
         }
 
     }
